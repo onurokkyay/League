8cee2c10a84f6523455f22648ef16f69152785f7
onurokyay <onurokyay1@gmail.com>
2025-06-04 21:34:57 +0300

    GlobalExceptionHandler hidden for swagger

--- src/main/java/com/krawenn/lol/exception/GlobalExceptionHandler.java
+++ src/main/java/com/krawenn/lol/exception/GlobalExceptionHandler.java
@@ -1,33 +1,34 @@
 package com.krawenn.lol.exception;
 
 import com.krawenn.lol.dto.ErrorResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(RiotGamesBusinessException.class)
     public ResponseEntity<Object> handleBusinessException(RiotGamesBusinessException ex) {
-        logger.warn("RiotGamesBusinessException: {}", ex.getMessage(), ex);
+        log.warn("RiotGamesBusinessException: {}", ex.getMessage(), ex);
         return ResponseEntity.status(ex.getStatus())
                 .body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(RiotGamesSystemException.class)
     public ResponseEntity<Object> handleSystemException(RiotGamesSystemException ex) {
-        logger.error("RiotGamesSystemException: {}", ex.getMessage(), ex);
+        log.error("RiotGamesSystemException: {}", ex.getMessage(), ex);
         return ResponseEntity.status(ex.getStatus())
                 .body(new ErrorResponse("An error occurred while contacting Riot Games: " + ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleAllExceptions(Exception ex) {
-        logger.error("Internal server error: {}", ex.getMessage(), ex);
+        log.error("Internal server error: {}", ex.getMessage(), ex);
         return ResponseEntity.status(500)
                 .body(new ErrorResponse("Internal server error: " + ex.getMessage()));
     }
